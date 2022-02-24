fbd3cc9c48f3b7bebce6bccdc13a03c2bde9f572
carlo <stanciu.carlo@gmail.com>
2022-02-24 14:27:40 +0200

    upgraded framework version 1.1.1 + refactor in args

--- src/main/java/software/crldev/elrondspringbootdemo/dto/FunctionDto.java
+++ src/main/java/software/crldev/elrondspringbootdemo/dto/FunctionDto.java
@@ -6,12 +6,9 @@ import software.crldev.elrondspringbootstarterreactive.domain.account.Address;
 import software.crldev.elrondspringbootstarterreactive.domain.common.Balance;
 import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.ContractFunction;
 import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.ContractQuery;
-import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.FunctionArgs;
 import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.FunctionName;
 import software.crldev.elrondspringbootstarterreactive.domain.transaction.GasLimit;
 import software.crldev.elrondspringbootstarterreactive.domain.wallet.Wallet;
-
-import java.util.Arrays;
 
 import static java.util.Objects.nonNull;
 
@@ -23,14 +20,14 @@ public class FunctionDto {
     private Double value;
     private String funcName;
     private String gasLimit;
-    private String[] args;
+    private Object[] args;
 
     public ContractQuery mapQuery(Wallet wallet) {
         return ContractQuery.builder()
                 .callerAddress(Address.fromHex(wallet.getPublicKeyHex()))
                 .smartContractAddress(Address.fromBech32(scAddress))
                 .functionName(FunctionName.fromString(funcName))
-                .args(Arrays.asList(args).isEmpty() ? FunctionArgs.empty() : FunctionArgs.fromString(args))
+                .args(ArgUtils.processArgs(args))
                 .build();
     }
 
@@ -38,7 +35,7 @@ public class FunctionDto {
         return ContractFunction.builder()
                 .smartContractAddress(Address.fromBech32(scAddress))
                 .functionName(FunctionName.fromString(funcName))
-                .args(Arrays.asList(args).isEmpty() ? FunctionArgs.empty() : FunctionArgs.fromString(args))
+                .args(ArgUtils.processArgs(args))
                 .value(nonNull(value) ? Balance.fromEgld(value) : Balance.zero())
                 .gasLimit(nonNull(gasLimit) ? GasLimit.fromString(gasLimit) : GasLimit.defaultSmartContractCall())
                 .build();
--- src/main/java/software/crldev/elrondspringbootdemo/dto/esdt/ESDTTransferDto.java
+++ src/main/java/software/crldev/elrondspringbootdemo/dto/esdt/ESDTTransferDto.java
@@ -6,7 +6,6 @@ import software.crldev.elrondspringbootstarterreactive.domain.account.Address;
 import software.crldev.elrondspringbootstarterreactive.domain.common.Balance;
 import software.crldev.elrondspringbootstarterreactive.domain.esdt.ESDTTransfer;
 import software.crldev.elrondspringbootstarterreactive.domain.esdt.common.TokenIdentifier;
-import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.FunctionArgs;
 import software.crldev.elrondspringbootstarterreactive.domain.smartcontract.FunctionName;
 import software.crldev.elrondspringbootstarterreactive.domain.transaction.GasLimit;
 
@@ -14,7 +13,7 @@ import java.math.BigInteger;
 
 import static io.netty.util.internal.StringUtil.isNullOrEmpty;
 import static java.util.Objects.nonNull;
-import static org.springframework.util.ObjectUtils.isEmpty;
+import static software.crldev.elrondspringbootdemo.dto.ArgUtils.processArgs;
 
 @Data
 @NoArgsConstructor
@@ -24,7 +23,7 @@ public class ESDTTransferDto implements ESDTDto {
     private String tokenIdentifier;
     private BigInteger amount;
     private String functionName;
-    private String[] functionArgs;
+    private Object[] functionArgs;
     private String gasLimit;
 
 
@@ -35,7 +34,7 @@ public class ESDTTransferDto implements ESDTDto {
                 .tokenIdentifier(TokenIdentifier.fromString(tokenIdentifier))
                 .amount(Balance.fromNumber(amount))
                 .functionName(isNullOrEmpty(functionName) ? FunctionName.empty() : FunctionName.fromString(functionName))
-                .functionArgs(isEmpty(functionArgs) ? FunctionArgs.empty() : FunctionArgs.fromString(functionArgs))
+                .args(processArgs(functionArgs))
                 .gasLimit(nonNull(gasLimit) ? GasLimit.fromString(gasLimit) : GasLimit.defaultEsdtTransfer())
                 .build();
     }
